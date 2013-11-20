08a7399d86632a477dd859829d50b1b5859fd606
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2013-11-20 19:22:11 +0100

    Working!

--- DSA.hs
+++ DSA.hs
@@ -2,10 +2,9 @@ module DSA where
 
 import Data.Maybe (fromJust)
 import System.Random
-import System.IO.Unsafe (unsafePerformIO)
 
 import Math
-import qualified Primality
+import Primality
 
 type DSAParameters = 
   ( Integer -- ^ p - a prime modulus
@@ -23,21 +22,22 @@ type DSASignature =
   , Integer -- ^ s
   )
 
-genKey :: RandomGen g => g -> DSAParameters -> KeyPair
+genKey :: RandomGen g => g -> DSAParameters -> (g, KeyPair)
 genKey gen (p, q, g) | c > q-2   = genKey gen' (p, q, g)
-                     | otherwise = (x, y)
+                     | otherwise = (gen', (x, y))
   where n         = 160
         (c, gen') = randomR (2^(n-1), 2^n-1) gen
         x         = c + 1
         y         = powM p g x
 
 sign 
-  :: DSAParameters 
+  :: RandomGen g => g
+  -> DSAParameters 
   -> KeyPair 
   -> Integer -- ^ message digest
-  -> DSASignature
-sign (p, q, g) (x, _) z = (r, s)
-  where k = unsafePerformIO $ randomRIO (1, q - 1) -- TODO: Don't use unsafePerformIO
+  -> (g, DSASignature)
+sign gen (p, q, g) (x, _) z = (gen', (r, s))
+  where (k, gen') = randomR (1, q - 1) gen
         r = (powM p g k) `mod` q
         s = fromJust $ divM q (z + mulM q x r) k -- Safe since k is in Z*_q
 
@@ -53,10 +53,10 @@ verify (p, q, g) y z (r, s) = v == r
         u2 = mulM q r w
         w  = fromJust $ invM q s -- Safe since s is in Z*_q
 
-verifyParameters :: DSAParameters -> Bool
-verifyParameters (p, q, g) =
-  and [ isPrime p
-      , isPrime q
+verifyParameters :: RandomGen g => g -> DSAParameters -> Bool
+verifyParameters gen (p, q, g) =
+  and [ isPrime gen p
+      , isPrime gen q
       , hasBits 1024 p
       , hasBits 160 q
       , q `divides` (p - 1)
@@ -69,6 +69,3 @@ hasBits n x = 2 ^ (n - 1) <= x && x < 2 ^ n
 
 divides :: Integer -> Integer -> Bool
 divides a b = b `mod` a == 0
-
-isPrime :: Integer -> Bool
-isPrime n = Primality.isPrime (unsafePerformIO getStdGen) n -- TODO: Don't use unsafePerformIO
---
+++ Main.hs
@@ -0,0 +1,88 @@
+module Main where
+
+import Control.Monad (when)
+import Data.List (mapAccumL)
+import System.Exit
+import System.Random
+import Data.Text.Read (hexadecimal)
+import Data.Text (pack)
+
+import DSA (DSAParameters, KeyPair, DSASignature)
+import qualified DSA
+
+main = do
+  dsaParameters <- getDSAPArameters
+  checkDSAParameters dsaParameters
+
+  line <- getLine
+  (action line) dsaParameters
+
+action "genkey" = genKeys
+action "sign" = sign
+action "verify" = verify
+action _ = const $ return ()
+
+genKeys dsaParameters = do
+  n <- getNumber
+  gen <- getStdGen
+
+  loop n gen
+  where
+    loop 0 _ = return ()
+    loop n gen = do
+      let (gen', keyPair) = DSA.genKey gen dsaParameters
+      putStrLn $ showKeyPair keyPair
+      loop (n-1) gen'
+
+sign dsa = do
+  x <- getNumber
+  y <- getNumber
+  ds <- getContents
+
+  gen <- getStdGen
+
+  let zs = map (hexadecimal . pack) $ lines ds
+  let (_, ss) = mapAccumL (\gen (Right (z, _)) -> DSA.sign gen dsa (x, y) z) gen zs
+
+  putStr . unlines $ map showDSASignature ss
+
+verify dsa = do
+  y <- getNumber
+  signs <- getContents
+
+  loop y $ lines signs
+  where
+    loop _ [] = return ()
+    loop y (d':r':s':ss) = do
+      let (Right (d, _)) = hexadecimal . pack . drop 2 $ d'
+      let r = read . drop 2 $ r'
+      let s = read . drop 2 $ s'
+      putStrLn $ if DSA.verify dsa y d (r, s) then "signature_valid" else "signature_invalid"
+      loop y ss
+
+getDSAPArameters :: IO DSAParameters
+getDSAPArameters = do
+  p <- getNumber
+  q <- getNumber
+  g <- getNumber
+  return (p, q, g)
+
+checkDSAParameters :: DSAParameters -> IO ()
+checkDSAParameters dsaParameters = do
+  gen <- getStdGen
+  when (not $ DSA.verifyParameters gen dsaParameters) $ do
+    putStrLn "invalid_group"
+    exitFailure
+
+  putStrLn "valid_group"
+
+getNumber :: IO Integer
+getNumber = getLine >>= return . read . drop 2
+
+showKeyPair :: KeyPair -> String
+showKeyPair (x, y) = 
+  concat ["x=", show x, "\ny=", show y]
+
+showDSASignature :: DSASignature -> String
+showDSASignature (r, s) = 
+  concat ["r=", show r, "\ns=", show s]
